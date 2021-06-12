e003bf6334ccd3a3bfcf3c21af1adc2956262445
gavanderhoorn <g.a.vanderhoorn@tudelft.nl>
2021-06-12 19:41:34 +0200

    Cleanup imports, whitespace, unused variables

--- nodes/all_in_one_publisher.py
+++ nodes/all_in_one_publisher.py
@@ -22,34 +22,19 @@ import math
 import sys
 import win32pipe, win32file, pywintypes
 import os
-import os.path
-import struct
-
-import subprocess as sp
 
 # common ROS modules
-import roslib
 import rospy
 from rospy_message_converter import json_message_converter
 
 # module for Sim time 
 from rosgraph_msgs.msg import Clock
 
-
 # modules for Odom
-import numpy as np
 from nav_msgs.msg import Odometry
-from std_msgs.msg import String
-from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
-from geometry_msgs.msg import TransformStamped
-from geometry_msgs.msg import Transform, PoseStamped
-import tf2_ros
-
 
 # modules for laser scan
 from sensor_msgs.msg import LaserScan
-from tf_conversions import transformations
-
 
 # modules for imu
 from sensor_msgs.msg import Imu
@@ -57,28 +42,18 @@ from sensor_msgs.msg import Imu
 # module for tf
 from tf2_msgs.msg import TFMessage
 
+
 class ROSMessagePublisher:
-    
-    
     # some class variables shared across all instances of the class
 
     # sim time message initialization 
     pub_sim = rospy.Publisher('clock', Clock, queue_size=10)  
- 
 
     # odom messages initialization
     pub_odom = rospy.Publisher('odom', Odometry, queue_size=10)
-    transformStamped = t = TransformStamped()
-    # instantiate an object of transformbroadcaster
-    odom_broadcaster = tf2_ros.TransformBroadcaster()
-    base_footprint = Odometry()
-
 
     # laser scan messages initialization
     pub_laser = rospy.Publisher('scan', LaserScan, queue_size=10)  
-    t_laser = TransformStamped()
-    # instantiate an object of transformbroadcaster
-    laser_scan_broadcaster = tf2_ros.TransformBroadcaster()
 
     # instantiate object of imu publisher
     pub_imu = rospy.Publisher('imu', Imu, queue_size=10)  
@@ -86,13 +61,10 @@ class ROSMessagePublisher:
     # instantiate object of tf publisher
     pub_tf = rospy.Publisher('tf', TFMessage, queue_size=10)  
 
-
     def laser_scan_pub(self, scan):
-
         for count, value in enumerate(scan.ranges):
             if float(value) == 1000:
                 scan.ranges[count] = float('Inf')
-                
         self.pub_laser.publish(scan)
 
 
@@ -127,23 +99,19 @@ if __name__ == '__main__':
             sys.exit(1)
         else:
             print("symbolic link has already been created")
-        
+
         # wait a bit to ensure that symbolic links have been created
         time.sleep(1)
-        
-        object_class = ROSMessagePublisher()
 
+        object_class = ROSMessagePublisher()
 
         rospy.init_node('ros_publisher', anonymous=True)
         pipe = create_pipe("ROS_messages")
         print("waiting for client from FarmSim19")
         win32pipe.ConnectNamedPipe(pipe, None)
         print("got client from game!!")
-        
-     
 
         while not rospy.is_shutdown():
-           
             resp = win32file.ReadFile(pipe, 64*1024)
             #  convert json data from lua to ros message (data read from the pipe is in bytes: need to convert to string)
 
@@ -151,11 +119,11 @@ if __name__ == '__main__':
             if msg_list[0] == "rosgraph_msgs/Clock":
                 sim_time_msg = json_message_converter.convert_json_to_ros_message('rosgraph_msgs/Clock', msg_list[1])
                 object_class.pub_sim.publish(sim_time_msg)
-                
+
             elif msg_list[0] == "nav_msgs/Odometry":
                 odom_msg = json_message_converter.convert_json_to_ros_message('nav_msgs/Odometry', msg_list[1])
                 object_class.pub_odom.publish(odom_msg)
-            
+
             elif msg_list[0] == "sensor_msgs/LaserScan":
                 scan_msg = json_message_converter.convert_json_to_ros_message('sensor_msgs/LaserScan', msg_list[1])
                 object_class.laser_scan_pub(scan_msg)
@@ -163,11 +131,11 @@ if __name__ == '__main__':
             elif msg_list[0] == "sensor_msgs/Imu":
                 imu_msg = json_message_converter.convert_json_to_ros_message('sensor_msgs/Imu', msg_list[1])
                 object_class.pub_imu.publish(imu_msg)
-       
+
             elif msg_list[0] == "tf2_msgs/TFMessage":
                 tf_msg = json_message_converter.convert_json_to_ros_message('tf2_msgs/TFMessage', msg_list[1])
                 object_class.pub_tf.publish(tf_msg)
-           
+
 
     except rospy.ROSInterruptException:
         pass
